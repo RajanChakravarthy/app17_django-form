b71168ccf848e98f342976c658beddebf8668cf3
rajan Mahadevan <rajanchakravarthy@gmail.com>
2024-02-17 22:16:51 +0100

    Application form created and the date enter on the website is stored into database of processing.

---
+++ job_application/views.py
@@ -0,0 +1,23 @@
+from django.shortcuts import render
+from .forms import ApplicationFrom
+from .models import Form
+from django.contrib import messages
+
+def index(request):
+    if request.method == 'POST':
+        form = ApplicationFrom(request.POST)
+        if form.is_valid():
+            first_name = form.cleaned_data['first_name']
+            last_name = form.cleaned_data['last_name']
+            email = form.cleaned_data['email']
+            date = form.cleaned_data['date']
+            occupation = form.cleaned_data['occupation']
+
+            Form.objects.create(first_name=first_name, last_name=last_name,
+                                email=email, date=date, occupation=occupation)
+
+            messages.success(request, "From Submitted Successfully!")
+
+    return render(request, 'index.html')
+
+
